f433859941c5f42f49b2d2ca26aed6b7729b5d40
amure <woki.alder@gmail.com>
2012-10-31 22:10:27 +0800

    A second form of Main()
    给main函数添加args[]参数列

--- wc.cs
+++ wc.cs
@@ -2,14 +2,33 @@
 using System;
 public class WordCountEntry
 {
-	static public void Main()
+	static public void Main( string [] args)
 	{
 		Console.WriteLine("Beginning WordCount program ... ");
 
+		//程序首先添加接受参数的功能，参见12页
+		if (args.Length == 0 )
+		{
+			display_usage();//This function code is in P18.
+			return;
+		}
+		
 		WordCount theObj = new WordCount();
 		theObj.processFile();
 
 		Console.WriteLine( "Ending WordCount program ... ");
+	}
+
+	//copy from p18
+	public static void display_usage()
+	{
+		string usage =
+			@"usage: WordCount [-s] [-t] [-h] textfile.txt
+			where [] indicates an optional argument
+			-s prints a series of performance measurements
+			-t prints a tracce of the program
+			-h prints this message";
+		Console.WriteLine( usage );
 	}
 }
 
