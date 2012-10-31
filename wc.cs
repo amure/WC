d79143ff51b5c6bae534ffade6b65571512e80db
amure <woki.alder@gmail.com>
2012-10-31 21:45:59 +0800

    A p63 ver
    基础框架，后面的在此基础上做更改

--- wc.cs
+++ wc.cs
@@ -1,1 +1,46 @@
 // It is the project entry. form CPAPA p.63 CN.ver
+using System;
+public class WordCountEntry
+{
+	static public void Main()
+	{
+		Console.WriteLine("Beginning WordCount program ... ");
+
+		WordCount theObj = new WordCount();
+		theObj.processFile();
+
+		Console.WriteLine( "Ending WordCount program ... ");
+	}
+}
+
+public class WordCount
+{
+	public void processFile()
+	{
+		openFiles();
+		readFiles();
+		countWords();
+		writeWords();
+	}
+
+	private void countWords()
+	{
+		Console.WriteLine( "!!! WordCount.countWords() " );
+	}
+	
+		
+	private void readFiles()
+	{
+		Console.WriteLine( "!!! WordCount.readFiles() " );
+	}
+	
+	private void openFiles()
+	{
+		Console.WriteLine( "!!! WordCount.openFiles() " );
+	}
+	
+	private void writeWords()
+	{
+		Console.WriteLine( "!!! WordCount.writeWords() " );
+	}
+}
