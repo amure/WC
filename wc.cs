4bd94cc1a88e0dc993e91c62ce8f338cd991c286
amure <woki.alder@gmail.com>
2012-10-31 22:43:42 +0800

    参数的基础判断
    按照14页开始添加一些基础判断
    由于cvft现在还没有，所有改为打印之
    为了可以更好的在控制台看见结果，增加了两个参数的判断并显示的语句

--- wc.cs
+++ wc.cs
@@ -11,8 +11,34 @@ public class WordCountEntry
 		{
 			display_usage();//This function code is in P18.
 			return;
+		}else{	//There is a code block about foreach in P14
+			bool traceOn = false;
+			bool spyOn = false;
+
+			foreach ( string option in args )
+			{
+				if ( option.Equals ( "-t" ))
+					traceOn = true;
+				else if (option.Equals ( "-s" ))
+					spyOn = true;
+				else if (option.Equals ( "-h" ))
+				{display_usage(); return;}
+				else
+					Console.WriteLine( @"check_valid_file_type( option );" );
+			}
+		//} this is error if there is a }
+		//i want to read traceOn & spyON
+		if ( traceOn ) 
+			Console.WriteLine( "traceOn = true" );
+		else
+			Console.WriteLine( "traceOn = false" );
+		if ( spyOn ) 
+			Console.WriteLine( "spyOn = true" );
+		else
+			Console.WriteLine( "spyOn = false" );
 		}
-		
+
+
 		WordCount theObj = new WordCount();
 		theObj.processFile();
 
