de071f836fd4fe54d8ccf7e34fa1f15d40038d90
amure <woki.alder@gmail.com>
2012-11-01 00:49:34 +0800

    读取文件功能初始
    更具至19页的code以及上下文更改原始代码
    现在基本实现功能
    1、读取任意指定的ascii码文件，然后写如指定的wc.diag中
    2、-s，-t，-h的逻辑开关上一般已经实现
    但是中文的写入时大量的乱码，编码问题，当前不做修改考虑

--- wc.cs
+++ wc.cs
@@ -1,9 +1,14 @@
 // It is the project entry. form CPAPA p.63 CN.ver
 using System;
+using System.IO;// it is need to read & write a file p17
 public class WordCountEntry
 {
 	static public void Main( string [] args)
 	{
+		bool traceOn = false; //There is a code block about foreach in P14
+		bool spyOn = false;
+		string file_name = null; //code from p17 
+		
 		Console.WriteLine("Beginning WordCount program ... ");
 
 		//程序首先添加接受参数的功能，参见12页
@@ -12,8 +17,10 @@ public class WordCountEntry
 			display_usage();//This function code is in P18.
 			return;
 		}else{	//There is a code block about foreach in P14
-			bool traceOn = false;
-			bool spyOn = false;
+			//bool traceOn = false;
+			//bool spyOn = false;
+			//写这个区块应该是明显错了，事实上，上面两个布尔量也最好不写在这里
+			//string file_name = null; //code from p17
 
 			foreach ( string option in args )
 			{
@@ -24,8 +31,13 @@ public class WordCountEntry
 				else if (option.Equals ( "-h" ))
 				{display_usage(); return;}
 				else
+				{
 					Console.WriteLine( @"check_valid_file_type( option );" );
+					file_name = option;//transmit filename string into file_name
+				}
+				
 			}
+		}
 		//} this is error if there is a }
 		//i want to read traceOn & spyON
 		if ( traceOn ) 
@@ -36,11 +48,14 @@ public class WordCountEntry
 			Console.WriteLine( "spyOn = true" );
 		else
 			Console.WriteLine( "spyOn = false" );
-		}
+		//}
 
 
 		WordCount theObj = new WordCount();
-		theObj.processFile();
+		//transmit filename into processFile
+		//theObj.processFile();这里选择传递给这个函数，传递给构造函数的等到了后面在考虑
+		//现在这个方式比较简单
+		theObj.processFile( file_name );
 
 		Console.WriteLine( "Ending WordCount program ... ");
 	}
@@ -60,9 +75,11 @@ public class WordCountEntry
 
 public class WordCount
 {
-	public void processFile()
+	StreamReader freader = null;
+	StreamWriter fwriter = null;
+	public void processFile(string theFile)//accept a filename from WCE
 	{
-		openFiles();
+		openFiles( theFile );
 		readFiles();
 		countWords();
 		writeWords();
@@ -77,11 +94,31 @@ public class WordCount
 	private void readFiles()
 	{
 		Console.WriteLine( "!!! WordCount.readFiles() " );
+		//read freader that is a StramReader p18
+		string text_line;
+		while (( text_line = freader.ReadLine() ) != null )
+		{
+			//write to output file
+			fwriter.WriteLine ( text_line );
+		}
+		//must explicitly close the readers
+		freader.Close();
+		fwriter.Close();
+		//end in p18
 	}
 	
-	private void openFiles()
+	private void openFiles(string theFile)//accept a filename from pF
 	{
 		Console.WriteLine( "!!! WordCount.openFiles() " );
+
+		//copy code from p17
+		//StreamReader freader = File.OpenText ( file_name );
+		freader = File.OpenText ( theFile ); 
+		//StreamWriter fwriter = File.CreateText ( "wc.diag" );
+		fwriter = File.CreateText ( "wc.diag" );
+		
+		//这两个数据成员最简单的就是传递给readFiles，在rF里先实现p18，19里最简单的基础操作
+		//先不知用后面教授的方法，就在当前结构下，最简单的解决方法，就是提升freader&fw的访问级别
 	}
 	
 	private void writeWords()
